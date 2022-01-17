c3bab07083dac93223571150e0037228a03ae278
JakWai01 <jakob.waibel@web.de>
2022-01-17 14:38:17 +0100

    fix: Command structure

---
+++ cmd/sile-fystem/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/JakWai01/sile-fystem/cmd/sile-fystem/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/JakWai01/sile-fystem/cmd"
-
-func main() {
-	cmd.Execute()
-}
